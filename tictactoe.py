d053e54df2167f3ecce8e7fb18b167e81401a62f
ihavegerms <wdarrenc@hotmail.com>
2015-12-17 13:34:23 -0600

    Tic Tac Toe

---
+++ tictactoe.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+__author__ = 'Darren Carpenter'
+__license__ = 'GPL'
+__version__ = '1.0.1'
+__email__ = 'germz@protonmail.ch'
+
+# Notes: make it so you can play it one player
+
+import sys
+import itertools
+
+firstplayer = raw_input('What is the name of player one: ')
+secondplayer = raw_input('What is the name of player two: ')
+available = ['1','2','3','4','5','6','7','8','9']
+firstmoves = []
+secondmoves = []
+wincollection = [('1','2','3'), ('4','5','6'), ('7','8','9'), 
+                 ('1','4','7'), ('2','5','8'), ('3','6','9'),
+                 ('1','5','9'), ('7','5','3'), ('3','2','1'),
+                 ('6','5','4'), ('9','8','7'), ('7','4','1'),
+                 ('8','5','2'), ('9','6','3'), ('9','5','1'),
+                 ('3','5','7')]
+board = '''
+
+_1_|_2_|_3_
+_4_|_5_|_6_
+ 7 | 8 | 9 
+
+'''
+
+print '''
+
+############################
+# Tic Tac Toe - by: Darren #
+############################
+
+Rules: Select the position for your move by choosing the number in the
+       corresponding space.
+
+_1_|_2_|_3_
+_4_|_5_|_6_
+ 7 | 8 | 9 
+
+'''
+
+# the loop will continue until the available moves are exhausted
+while len(available) >= 1:
+    win1 = [] 
+    win2 = [] 
+
+    try:
+        ''' after 3 moves, begin appending all combinations of 3 
+            to the respective list '''
+        win1.append(list(itertools.combinations(firstmoves, 3)))
+        win2.append(list(itertools.combinations(secondmoves, 3)))
+
+        if len(available) <= 6:
+            ''' if any of the combinations in either list exist 
+                within wincollection '''
+            x = set(win1[0]) & set(wincollection)
+            y = set(win2[0]) & set(wincollection)
+            if len(list(x)) >= 1:
+                    winner = firstplayer
+                    print winner + ', you have won the game!'
+                    sys.exit(0)
+            elif len(list(y)) >= 1:
+                    winner = secondplayer
+                    print winner + ', you have won the game!'
+                    sys.exit(0)
+       
+        move1 = raw_input(firstplayer + ', your move: ')
+        available.remove(move1)
+        firstmoves.append(move1)
+        board = str.replace(board, move1, "X", 1)
+        print board
+        move2 = raw_input(secondplayer + ', your move: ')
+        available.remove(move2)
+        secondmoves.append(move2)
+        board = str.replace(board, move2, "O", 1)
+        print board
+
+    except ValueError:
+        print "You made an invalid choice."
+        continue
+    except KeyboardInterrupt:
+        quit = raw_input('\nWould you like to quit? [y/n] ')
+        if quit.lower() == "y":
+            sys.exit(0)
+        if quit.lower() == "n":
+            continue
